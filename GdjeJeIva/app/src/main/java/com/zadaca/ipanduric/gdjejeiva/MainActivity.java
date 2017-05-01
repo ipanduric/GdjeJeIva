7e53401c30dba1ae8bf091f633528599863c1f21
xivaa <ipanduric1@gmail.com>
2017-05-01 12:43:00 +0200

    Placing markers on different locations

--- GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
+++ GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
@@ -8,16 +8,20 @@ import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.PermissionRequest;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,10 +40,19 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.location.LocationListener;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+// import permissions.dispatcher.NeedsPermission;
+// import permissions.dispatcher.RuntimePermissions;
+
+// @RuntimePermissions
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks,
 GoogleApiClient.OnConnectionFailedListener {
 
@@ -51,8 +64,10 @@ GoogleApiClient.OnConnectionFailedListener {
     Marker mCurrLocationMarker;
     TextView tvLocationInfo;
     ImageButton ibTakePhoto;
+    String mCurrentPhotoPath;
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
 
     @Override
@@ -63,6 +78,77 @@ GoogleApiClient.OnConnectionFailedListener {
         mapFrag.getMapAsync(this);
         this.SetUpUI();
     }
+/*
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void startCamera() {
+        try {
+            dispatchTakePictureIntent();
+        } catch (IOException e) {
+        }
+    }
+
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("Access to External Storage is required")
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+*/
+    //https://developer.android.com/training/camera/photobasics.html#TaskPath
+/*
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+       //         ".jpg",         /* suffix */
+        //        storageDir      /* directory */
+       // );
+
+
+        // Save a file: path for use with ACTION_VIEW intents
+     //   mCurrentPhotoPath = image.getAbsolutePath();
+      //  return image;
+   // }
+
 
 
     private void SetUpUI() {
@@ -79,12 +165,14 @@ GoogleApiClient.OnConnectionFailedListener {
                     requestPermissions(new String[]{Manifest.permission.CAMERA},
                             MY_CAMERA_REQUEST_CODE);
                 }
+            //    MainActivityPermissionsDispatcher.startCameraWithCheck(this);
+
             }
         });
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); //http://stackoverflow.com/questions/34348552/how-to-add-a-satellite-view-in-android-studio
 
@@ -104,6 +192,48 @@ GoogleApiClient.OnConnectionFailedListener {
             buildGoogleApiClient();
             mGoogleMap.setMyLocationEnabled(true);
         }
+
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+
+                mGoogleMap.clear();
+                      MarkerOptions marker = new MarkerOptions().position(
+                        new LatLng(point.latitude, point.longitude)).title("Pozicija markera");
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+                mGoogleMap.addMarker(marker);
+//stackoverflow.com/questions/17143129/add-marker-on-android-google-map-via-touch-or-tap
+            }
+        });
+
+        mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                tvLocationInfo.setText("");
+                LatLng position = marker.getPosition();
+
+                String message = "Geografska širina: " + position.latitude + "°" + "\nGeografska dužina: " + position.longitude + "°\n ";
+                tvLocationInfo.setText(message);
+                if (Geocoder.isPresent()) {
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> nearByAddresses = geocoder.getFromLocation(position.latitude,position.longitude,1);
+                        if(nearByAddresses.size() > 0) {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            android.location.Address nearestAddress = nearByAddresses.get(0);
+                            stringBuilder.append(nearestAddress.getAddressLine(0)).append("\n")
+                                    .append(nearestAddress.getLocality()).append("\n")
+                                    .append(nearestAddress.getCountryName());
+                            tvLocationInfo.append(stringBuilder.toString());
+                        }
+                    } catch (IOException e) { e.printStackTrace(); }
+                }
+
+
+                return false;
+            }
+        });
+
     }
 
 
@@ -138,6 +268,8 @@ GoogleApiClient.OnConnectionFailedListener {
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+
 
     @Override
     public void onLocationChanged(Location location) {
@@ -174,6 +306,7 @@ GoogleApiClient.OnConnectionFailedListener {
         }
 
     }
+
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
@@ -213,10 +346,38 @@ GoogleApiClient.OnConnectionFailedListener {
         }
     }
 
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            // Show the thumbnail on ImageView
+            Uri imageUri = Uri.parse(mCurrentPhotoPath);
+            File file = new File(imageUri.getPath());
+            try {
+                InputStream ims = new FileInputStream(file);
+                ivPreview.setImageBitmap(BitmapFactory.decodeStream(ims));
+            } catch (FileNotFoundException e) {
+                return;
+            }
+
+            // ScanFile so it will be appeared on Gallery
+            MediaScannerConnection.scanFile(MainActivity.this,
+                    new String[]{imageUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        }
+    }*/
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+     //   MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
