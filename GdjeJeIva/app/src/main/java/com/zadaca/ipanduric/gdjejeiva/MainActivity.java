54693093a4ed3da74ad6b307b2d1d6ced95450cc
xivaa <ipanduric1@gmail.com>
2017-05-01 15:02:50 +0200

    Sound added

--- GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
+++ GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -20,6 +22,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.ImageButton;
@@ -46,13 +49,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-// import permissions.dispatcher.NeedsPermission;
-// import permissions.dispatcher.RuntimePermissions;
 
-// @RuntimePermissions
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks,
 GoogleApiClient.OnConnectionFailedListener {
 
@@ -65,6 +66,9 @@ GoogleApiClient.OnConnectionFailedListener {
     TextView tvLocationInfo;
     ImageButton ibTakePhoto;
     String mCurrentPhotoPath;
+    SoundPool mSoundPool;
+    boolean mLoaded = false;
+    HashMap<Integer, Integer> mSoundMap = new HashMap<>();
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
     static final int REQUEST_TAKE_PHOTO = 1;
@@ -77,77 +81,8 @@ GoogleApiClient.OnConnectionFailedListener {
         mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fGoogleMap);
         mapFrag.getMapAsync(this);
         this.SetUpUI();
+        this.loadSounds();
     }
-/*
-    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void startCamera() {
-        try {
-            dispatchTakePictureIntent();
-        } catch (IOException e) {
-        }
-    }
-
-    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void showRationaleForCamera(final PermissionRequest request) {
-        new AlertDialog.Builder(this)
-                .setMessage("Access to External Storage is required")
-                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        request.proceed();
-                    }
-                })
-                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        request.cancel();
-                    }
-                })
-                .show();
-    }
-
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-*/
-    //https://developer.android.com/training/camera/photobasics.html#TaskPath
-/*
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-       //         ".jpg",         /* suffix */
-        //        storageDir      /* directory */
-       // );
-
-
-        // Save a file: path for use with ACTION_VIEW intents
-     //   mCurrentPhotoPath = image.getAbsolutePath();
-      //  return image;
-   // }
 
 
 
@@ -165,10 +100,24 @@ GoogleApiClient.OnConnectionFailedListener {
                     requestPermissions(new String[]{Manifest.permission.CAMERA},
                             MY_CAMERA_REQUEST_CODE);
                 }
-            //    MainActivityPermissionsDispatcher.startCameraWithCheck(this);
-
             }
         });
+    }
+
+    private void loadSounds() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.mSoundPool = new SoundPool.Builder().setMaxStreams(10).build();
+        } else {
+            this.mSoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        }
+        this.mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("Test", String.valueOf(sampleId));
+                mLoaded = true;
+            }
+        });
+        this.mSoundMap.put(R.raw.whip, this.mSoundPool.load(this, R.raw.whip,1));
     }
 
     @Override
@@ -193,46 +142,6 @@ GoogleApiClient.OnConnectionFailedListener {
             mGoogleMap.setMyLocationEnabled(true);
         }
 
-        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-
-                mGoogleMap.clear();
-                      MarkerOptions marker = new MarkerOptions().position(
-                        new LatLng(point.latitude, point.longitude)).title("Pozicija markera");
-                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
-                mGoogleMap.addMarker(marker);
-//stackoverflow.com/questions/17143129/add-marker-on-android-google-map-via-touch-or-tap
-            }
-        });
-
-        mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                tvLocationInfo.setText("");
-                LatLng position = marker.getPosition();
-
-                String message = "Geografska širina: " + position.latitude + "°" + "\nGeografska dužina: " + position.longitude + "°\n ";
-                tvLocationInfo.setText(message);
-                if (Geocoder.isPresent()) {
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        List<Address> nearByAddresses = geocoder.getFromLocation(position.latitude,position.longitude,1);
-                        if(nearByAddresses.size() > 0) {
-                            StringBuilder stringBuilder = new StringBuilder();
-                            android.location.Address nearestAddress = nearByAddresses.get(0);
-                            stringBuilder.append(nearestAddress.getAddressLine(0)).append("\n")
-                                    .append(nearestAddress.getLocality()).append("\n")
-                                    .append(nearestAddress.getCountryName());
-                            tvLocationInfo.append(stringBuilder.toString());
-                        }
-                    } catch (IOException e) { e.printStackTrace(); }
-                }
-
-
-                return false;
-            }
-        });
 
     }
 
@@ -305,6 +214,51 @@ GoogleApiClient.OnConnectionFailedListener {
             } catch (IOException e) { e.printStackTrace(); }
         }
 
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+
+                int soundID = mSoundMap.get(R.raw.whip);
+                mSoundPool.play(soundID, 1,1,1,0,1f);
+
+                mGoogleMap.clear();
+                MarkerOptions marker = new MarkerOptions().position(
+                        new LatLng(point.latitude, point.longitude)).title("Pozicija markera");
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+                mGoogleMap.addMarker(marker);
+//stackoverflow.com/questions/17143129/add-marker-on-android-google-map-via-touch-or-tap
+            }
+        });
+
+        mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                tvLocationInfo.setText("");
+                LatLng position = marker.getPosition();
+
+                String message = "Geografska širina: " + position.latitude + "°" + "\nGeografska dužina: " + position.longitude + "°\n ";
+                tvLocationInfo.setText(message);
+                if (Geocoder.isPresent()) {
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> nearByAddresses = geocoder.getFromLocation(position.latitude,position.longitude,1);
+                        if(nearByAddresses.size() > 0) {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            android.location.Address nearestAddress = nearByAddresses.get(0);
+                            stringBuilder.append(nearestAddress.getAddressLine(0)).append("\n")
+                                    .append(nearestAddress.getLocality()).append("\n")
+                                    .append(nearestAddress.getCountryName());
+                            tvLocationInfo.append(stringBuilder.toString());
+                        }
+                    } catch (IOException e) { e.printStackTrace(); }
+                }
+
+
+                return false;
+            }
+        });
+
+
     }
 
 
@@ -346,30 +300,6 @@ GoogleApiClient.OnConnectionFailedListener {
         }
     }
 
-    /*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            // Show the thumbnail on ImageView
-            Uri imageUri = Uri.parse(mCurrentPhotoPath);
-            File file = new File(imageUri.getPath());
-            try {
-                InputStream ims = new FileInputStream(file);
-                ivPreview.setImageBitmap(BitmapFactory.decodeStream(ims));
-            } catch (FileNotFoundException e) {
-                return;
-            }
-
-            // ScanFile so it will be appeared on Gallery
-            MediaScannerConnection.scanFile(MainActivity.this,
-                    new String[]{imageUri.getPath()}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                        }
-                    });
-        }
-    }*/
 
 
     @Override
