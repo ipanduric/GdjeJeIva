6d7b1973bad24e9788b809f581b0b22833aed88d
xivaa <ipanduric1@gmail.com>
2017-05-07 22:07:33 +0200

    App final version

--- GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
+++ GdjeJeIva/app/src/main/java/com/zadaca/ipanduric/gdjejeiva/MainActivity.java
@@ -1,17 +1,24 @@
 package com.zadaca.ipanduric.gdjejeiva;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.media.AudioManager;
+import android.media.MediaScannerConnection;
+import android.media.RingtoneManager;
 import android.media.SoundPool;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -21,17 +28,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
-import android.webkit.PermissionRequest;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.Manifest;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
@@ -41,19 +47,22 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.location.LocationListener;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
 
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks,
 GoogleApiClient.OnConnectionFailedListener {
 
@@ -65,13 +74,14 @@ GoogleApiClient.OnConnectionFailedListener {
     Marker mCurrLocationMarker;
     TextView tvLocationInfo;
     ImageButton ibTakePhoto;
-    String mCurrentPhotoPath;
     SoundPool mSoundPool;
     boolean mLoaded = false;
     HashMap<Integer, Integer> mSoundMap = new HashMap<>();
-
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
     private static final int MY_CAMERA_REQUEST_CODE = 100;
-    static final int REQUEST_TAKE_PHOTO = 1;
+    StringBuilder stringBuilder;
+    Uri photoURI;
 
 
     @Override
@@ -84,8 +94,6 @@ GoogleApiClient.OnConnectionFailedListener {
         this.loadSounds();
     }
 
-
-
     private void SetUpUI() {
         ibTakePhoto = (ImageButton) this.findViewById(R.id.ibTakePhoto);
         tvLocationInfo = (TextView) this.findViewById(R.id.tvLocationInfo);
@@ -94,16 +102,16 @@ GoogleApiClient.OnConnectionFailedListener {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-
-                if (checkSelfPermission(Manifest.permission.CAMERA)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA},
-                            MY_CAMERA_REQUEST_CODE);
+                if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
                 }
+                MainActivityPermissionsDispatcher.startCameraWithCheck(MainActivity.this);
+
             }
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.FROYO)
     private void loadSounds() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             this.mSoundPool = new SoundPool.Builder().setMaxStreams(10).build();
@@ -142,9 +150,7 @@ GoogleApiClient.OnConnectionFailedListener {
             mGoogleMap.setMyLocationEnabled(true);
         }
 
-
     }
-
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -177,8 +183,6 @@ GoogleApiClient.OnConnectionFailedListener {
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
-
-
 
     @Override
     public void onLocationChanged(Location location) {
@@ -257,10 +261,7 @@ GoogleApiClient.OnConnectionFailedListener {
                 return false;
             }
         });
-
-
     }
-
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
@@ -300,14 +301,12 @@ GoogleApiClient.OnConnectionFailedListener {
         }
     }
 
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-     //   MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+        MainActivityPermissionsDispatcher.startCameraWithCheck(MainActivity.this);
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
@@ -334,22 +333,105 @@ GoogleApiClient.OnConnectionFailedListener {
             }
             //http://stackoverflow.com/questions/38552144/how-get-permission-for-camera-in-android-specifically-marshmallow
 
-                case MY_CAMERA_REQUEST_CODE: {
 
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                        Toast.makeText(this, "Camera permission granted!", Toast.LENGTH_LONG).show();
-
-                    } else {
-
-                        Toast.makeText(this, "Camera permission denied!", Toast.LENGTH_LONG).show();
-
-                    }
                 return;
             }
         }
+
+
+    @NeedsPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void startCamera() {
+        try {
+            dispatchTakePictureIntent();
+        } catch (IOException e) {
+        }
+    }
+
+    @OnShowRationale(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("Access to External Storage needed")
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.cancel();
+                    }
+                })
+                .show();
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Uri imageUri = Uri.parse(mCurrentPhotoPath);
+            File file = new File(imageUri.getPath());
+            try {
+                InputStream inputStream = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                return;
+            }
+
+            MediaScannerConnection.scanFile(MainActivity.this,
+                    new String[]{imageUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+            sendNotification();
+        }
+    }
+
+    private void sendNotification() {
+        String msgText = "Picture saved!";
+        Intent notificationIntent = new Intent(Intent.ACTION_VIEW, photoURI);
+        notificationIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setAutoCancel(true)
+                .setContentTitle("Gdje je Iva?")
+                .setContentText(msgText)
+                .setSmallIcon(android.R.drawable.ic_dialog_email)
+                .setContentIntent(pendingIntent)
+                .setLights(Color.YELLOW, 2000, 1000)
+                .setVibrate(new long[]{1000,1000,1000,1000,1000})
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        Notification notification = notificationBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notification);
+        this.finish();
+    }
+
+    private File createImageFile() throws IOException {
+        String imageFileName = "JPEG_" + stringBuilder + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                return;
+            }
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", createImageFile());
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
 
 }
